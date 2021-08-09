55bff847608317e02a250fc5487898ff2ff3c718
Christopher Huls <christopherhuls@ChristophersMBP.attlocal.net>
2021-08-09 18:52:21 -0400

    removed unneeded comment

--- projects/01_fyyur/starter_code/forms.py
+++ projects/01_fyyur/starter_code/forms.py
@@ -5,11 +5,6 @@ from flask_wtf import Form, CSRFProtect
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
 from wtforms.validators import DataRequired, AnyOf, URL, Regexp, ValidationError, Optional
 
-#Recieved CSFR errors and used the following resources to resolve across my Forms.py, App.py and HTML files:
-#https://knowledge.udacity.com/questions/530612
-#https://stackoverflow.com/questions/21501058/form-validation-fails-due-missing-csrf
-#https://flask-wtf.readthedocs.io/en/latest/csrf/
-
 def validate_phone_number(form, field):
         regex = re.compile('^\\(?([0-9]{3})\\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$')
         print(str(field.data))
